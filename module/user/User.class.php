5391197cc5ebac53aa4bfa4d3df4fa11400acf63
yeqingyang <lpz8120903@gmail.com>
2014-08-14 08:55:34 +0800

    0814

--- module/user/User.class.php
+++ module/user/User.class.php
@@ -1,10 +1,18 @@
 <?php
 class User{
+	private $tbl_name="t_user";
 	public static function getUsers(){
-		
+		$data=new Data();
+		$query="select * from $this->tbl_name;";
+		$ret = $data->query($query);
+		return $ret;
 	}
 	
 	public static function addUser($user){
-		
+		$data=new Data();
+		$query="insert into $this->tbl_name values";
+		$query .= "(".$user['uid'].",".$user['name'].");";
+		$ret = $data->query($query);
+		return 'ok';
 	}
 }
