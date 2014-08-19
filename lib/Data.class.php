8384d78dcb060d4f8823ce951dbde21867606bc4
lpz <lpz@localhost.localdomain>
2014-08-19 06:37:30 -0700

    modified:   index.php 	modified:   lib/Data.class.php 	modified:   module/user/User.class.php

--- lib/Data.class.php
+++ lib/Data.class.php
@@ -1,16 +1,24 @@
 <?php
-define(ROOT, dirname(__FILE__));
-include_once ROOT.'/lib/Mysql.def.php';
+#define("ROOT", dirname(dirname(__FILE__)));
+require_once ROOT.'/def/Mysql.def.php';
 class Data{
 	private $mysqli;
 	public function Data(){
 		$this->mysqli=new mysqli(MysqlDef::MYSQL_DB_IP, MysqlDef::MYSQL_DB_USER, "", MysqlDef::MYSQL_DB_NAME);
+		if (mysqli_connect_error()) { 
+			die('Connect Error (' . mysqli_connect_errno() . ') '
+					. mysqli_connect_error()); 
+		} 
 	}
-	
+
 	public function init(){
 		$this->mysqli=new mysqli(MysqlDef::MYSQL_DB_IP, MysqlDef::MYSQL_DB_USER, "", MysqlDef::MYSQL_DB_NAME);
+		if (mysqli_connect_error()) { 
+			die('Connect Error (' . mysqli_connect_errno() . ') '
+					. mysqli_connect_error()); 
+		} 
 	}
-	
+
 	public function query($query){
 		if(empty($this->mysqli)){
 			$this->init();
@@ -24,13 +32,10 @@ class Data{
 				}
 			}
 		}
-		$result->free();
+		//	$result->free();
 		$this->mysqli->close();
-		return 'ok';
+		return $return;
 	}
 	function __destruct() {
-		if(!empty($this->mysqli)){
-			$this->mysqli->close();
-		}
 	}
 }
--- module/user/User.class.php
+++ module/user/User.class.php
@@ -1,18 +1,21 @@
 <?php
 class User{
-	private $tbl_name="t_user";
+	const tbl_name="t_user";
 	public static function getUsers(){
 		$data=new Data();
-		$query="select * from $this->tbl_name;";
+		$query="select * from ".self::tbl_name.";";
 		$ret = $data->query($query);
 		return $ret;
 	}
 	
 	public static function addUser($user){
 		$data=new Data();
-		$query="insert into $this->tbl_name values";
-		$query .= "(".$user['name'].");";
+		$cols = implode(',',array_keys($user));
+		$values = implode(',',$user);
+		$query="insert into ".self::tbl_name."(".$cols.") "." values"."(".$values.");";
+		echo $query;
 		$ret = $data->query($query);
-		return 'ok';
+		return $ret;
 	}
+
 }
