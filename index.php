8abceb76bae9eb8b6f2709bfd69e95f75945ae2d
yeqingyang <lpz8120903@163.com>
2014-08-20 16:18:38 +0800

    log

--- index.php
+++ index.php
@@ -26,5 +26,8 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 require './def/Classes.def.php';
 
 require_once './lib/MyAutoLoad.class.php';
+
+$filename="log/fb.log";
+Logger::init($filename, 1);
 // 亲^_^ 后面不需要任何代码了 就是如此简单
 RestUtils::processRequest();
