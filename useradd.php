3e6d9887d53bfa2a243043c2a3e841c9fa1fbff0
lpz <lpz8120903@163.com>
2014-08-21 17:17:46 -0700

    new file:   useradd.php 	modified:   lib/Data.class.php 	modified:   lib/RestUtils.class.php 	modified:   lib/Util.class.php 	modified:   module/user/OneUser.class.php

---
+++ useradd.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>用户添加</title>
+</head>
+<body>
+<form method="post" action="index.php">
+<table width="541" border="0"> 
+<tr> 
+<td width="26%">姓名：</td> 
+<td width="74%"><input type="text" name="username" value="raymond" id="username"/></td> 
+</tr> 
+<tr> 
+</table>
+<input type="submit" />
+</form>
+ 
+</body>
+</html>
