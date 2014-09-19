f536208e4f905783e72f7cc24526c7221ee9b95f
lpz <lpz8120903@163.com>
2014-09-18 17:45:09 -0700

    new file:   app/controllers/GroupController.php 	new file:   app/controllers/PostsController.php 	new file:   app/models/Group.php 	new file:   app/views/group/index.phtml 	new file:   app/views/index.phtml 	new file:   app/views/layouts/common.phtml 	new file:   app/views/layouts/posts.phtml 	new file:   app/views/posts/last.phtml 	new file:   app/views/posts/show.phtml

---
+++ app/controllers/GroupController.php
@@ -0,0 +1,34 @@
+<?php
+
+class SignupController extends Phalcon\Mvc\Controller
+{
+
+	public function indexAction()
+	{
+
+	}
+
+	public function registerAction()
+	{
+
+		//Request variables from html form
+		$name = $this->request->getPost('name', 'string');
+		$member = $this->request->getPost('member', 'string');
+
+		$group = new Group();
+		$group->name = $name;
+
+		//Store and check for errors
+		if ($user->save() == true) {
+			echo 'Thanks for register!</p>';
+			echo Phalcon\Tag::linkTo("index","Back");
+		} else {
+			echo 'Sorry, the next problems were generated: ';
+			foreach ($user->getMessages() as $message){
+				echo $message->getMessage(), '<br/>';
+			}
+			echo Phalcon\Tag::linkTo("index","Back");	
+		}
+	}
+
+}
