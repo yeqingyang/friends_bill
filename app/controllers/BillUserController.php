8cb15c24894200e6be3cfce5394789d56ce86903
yeqingyang <lpz8120903@163.com>
2014-10-21 11:31:49 +0800

    bill user

---
+++ app/controllers/BillUserController.php
@@ -0,0 +1,21 @@
+<?php
+/***************************************************************************
+ * 
+ * Copyright (c) 2010 babeltime.com, Inc. All Rights Reserved
+ * $Id$
+ * 
+ **************************************************************************/
+
+ /**
+ * @file $HeadURL$
+ * @author $Author$(liupengzhan@babeltime.com)
+ * @date $Date$
+ * @version $Revision$
+ * @brief 
+ *  
+ **/
+class BillUserController extends ControllerBase{
+	
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
